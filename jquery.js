6d3765a592ef6b353728add6202d89b572c903e2
Zuhdi-Hacker1 <65784682+Zuhdi-Hacker1@users.noreply.github.com>
2020-05-23 13:24:06 +0300

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,18 @@
+$("document").ready(function(){
+  $(".shownavbar").click(function(){
+    $("#div1").fadeIn();
+    $("#div2").fadeIn();
+    $("#div3").fadeIn();
+    $("#div4").fadeIn()
+  });
+  $(".hide").click(function(){
+    $("#div1").fadeOut();
+    $("#div2").fadeOut();
+    $("#div3").fadeOut();
+    $("#div4").fadeOut();
+  });
+});
+
+/*
+* YAYA!!!!
+*/
